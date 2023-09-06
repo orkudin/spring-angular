8ab02ac2a602c45b8dfe39982344e9944a2d0d78
orkudin <106586367+orkudin@users.noreply.github.com>
2023-09-07 03:50:10 +0600

    Front Side on Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PostService } from './post.service';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent],
+  imports: [BrowserModule, HttpClientModule, FormsModule],
+  providers: [PostService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/post.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Post } from './post';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class PostService {
+  private apiServerUrl = environment.apiBaseUrl;
+
+  constructor(private http: HttpClient) { }
+
+  public getPosts(): Observable<Post[]> {
+    return this.http.get<Post[]>(`${this.apiServerUrl}/post/all`)
+  }
+
+  public addPost(post: Post): Observable<Post> {
+    return this.http.post<Post>(`${this.apiServerUrl}/post/add`, post)
+  }
+
+  public updatePost(post: Post): Observable<Post> {
+    console.log("From method")
+    console.log(post)
+    return this.http.put<Post>(`${this.apiServerUrl}/post/update`, post)
+  }
+
+  public deletePost(postId: number): Observable<void> {//Ничего не возвращаем, поэтому void
+    return this.http.delete<void>(`${this.apiServerUrl}/post/delete/${postId}`)
+  }
+
+  public findPost(postId: number): Observable<Post> {
+    return this.http.get<Post>(`${this.apiServerUrl}/post/find/${postId}`)
+  }
+}
